4227387ae9044c84ef21ade388d65a7f0dc6a6cc
miker-mcd <michaelrmcd@gmail.com>
2018-06-18 07:00:00 -0700

    Add form and deploy site to test

--- script.js
+++ script.js
@@ -64,4 +64,33 @@ $(document).ready(function() {
         }
       }
     });
+
+  var formMessages = $('#form-messages');
+  var form = $('form');
+  $(form).submit(function(event) {
+    event.preventDefault();
+    var formData = $(form).serialize();
+    $.ajax({
+      type: 'POST',
+      url: $(form).attr('action'),
+      data: formData
+    })
+      .done(function(response) {
+        $(formMessages).removeClass('error');
+        $(formMessages).addClass('success');
+        $(formMessages).text(response);
+        $('#name').val('');
+        $('#email').val('');
+        $('#message').val('');
+      })
+      .fail(function(data) {
+        $(formMessages).removeClass('success');
+        $(formMessages).addClass('error');
+        if (data.responseText !== '') {
+          $(formMessages).text(data.responseText);
+        } else {
+          $(formMessages).text('Oops! An error occurred and your message could not be sent.');
+        }
+      });
+  });
 });
